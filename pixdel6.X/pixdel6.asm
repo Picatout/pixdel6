90d4bae18bacdc225824ab6fac3e721a488ff5e9
picatout <jd_temp@yahoo.fr>
2016-09-28 17:05:32 -0400

    remplaéc _WDTE_OFF par _WDTE_ON

--- pixdel6.X/pixdel6.asm
+++ pixdel6.X/pixdel6.asm
@@ -65,7 +65,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;    
     
     include p12f1572.inc
-    __config _CONFIG1, _FOSC_INTOSC&_WDTE_OFF&_PWRTE_OFF&_MCLRE_ON&_CP_OFF&_BOREN_OFF&_CLKOUTEN_OFF
+    __config _CONFIG1, _FOSC_INTOSC&_WDTE_ON&_PWRTE_OFF&_MCLRE_ON&_CP_OFF&_BOREN_OFF&_CLKOUTEN_OFF
     __config _CONFIG2, _WRT_ALL&_PLLEN_ON&_STVREN_OFF&_LVP_OFF
 
     radix dec
